56ba53b4f0aaf598211414b9ebe1826c773e0d7d
hllqk <shuia@shuia.tk>
2022-08-22 02:51:52 +0000

    s

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         ip=self.client_address[0]
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         data={'code':200,'msg':'成功','ip':ip}
         res=json.dumps(data)

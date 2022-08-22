16124612e8269d04eba88cd15594b80e81402b66
hllqk <shuia@shuia.tk>
2022-08-22 02:47:47 +0000

    fusd

--- api/index.py
+++ api/index.py
@@ -3,11 +3,11 @@ import json
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
-        #ip=self.client_address[0]
+        ip=self.client_address[0]
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        data={'code':200,'msg':'成功','ip':''}
-        res=json.dumps(data,ensure_ascii=False,indent=4)
-        self.wfile.write(res.encode('utf-8'))
+        data={'code':200,'msg':'成功','ip':ip}
+        res=json.dumps(data)
+        self.wfile.write(res.encode())
         return
